437fe07c0d21df86100d3220f85ef7d1261f33d1
agustin alfonsin <agustinalfonsin17@gmail.com>
2022-12-08 12:33:32 -0300

    finalizamos proyecto final

---
+++ js/products.js
@@ -0,0 +1,66 @@
+const productos = [
+    {
+     id: 1,
+     nombre: "lavandina" ,
+     precio: 180,
+     img:
+     "https://th.bing.com/th/id/OIP.F6w8dfvAtwDmI1zGcVCD-QHaHa?pid=ImgDet&rs=1",
+     cantidad: 1,
+     },
+     {
+     id: 2,
+     nombre: "cera" ,
+     precio: 150,
+     img:
+     "https://www.mimbral.cl/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/0/8/087012033.jpg",
+     cantidad: 1,
+     },
+     {
+     id: 3,
+     nombre: "detergente" ,
+     precio: 220,
+     img:
+     "https://th.bing.com/th/id/R.bb84a4cbfd57fbbedc89894bc331772c?rik=EwlVV%2bZT8FHlsw&pid=ImgRaw&r=0",
+     cantidad: 1,
+     },
+     {
+     id: 4,
+     nombre: "desengrasante" ,
+     precio: 350,
+     img:
+     "https://th.bing.com/th/id/R.8529f7453eaa115d5f4dcb2a58041a7c?rik=UQm8eaqpGLdH1w&pid=ImgRaw&r=0",
+     cantidad: 1,
+     },
+     {
+     id: 5,
+     nombre: "franela" ,
+     precio: 150,
+     img:
+     "https://production-tailoy-repo-magento-statics.s3.amazonaws.com/imagenes/872x872/productos/i/f/r/franela-amarilla-x-1mt-794-default-1.jpg",
+     cantidad: 1,
+     },
+     {
+     id: 6,
+     nombre: "trapo de piso" ,
+     precio: 180,
+     img:
+     "https://stock.com.py/images/thumbs/0166063.jpeg",
+     cantidad: 1,
+     },
+     {
+     id: 7,
+     nombre: "plumero" ,
+     precio: 450,
+     img:
+     "https://th.bing.com/th/id/OIP.zvHxVhEnTzcTPjqihGfgvgHaF6?pid=ImgDet&rs=1",
+     cantidad: 1,
+     },
+     {
+     id: 8,
+     nombre: "jabon liquido" ,
+     precio: 160,
+     img:
+     "https://unimarc.vteximg.com.br/arquivos/ids/165908-1000-1000/jabon-liquido-antibacterial-protex-221ml.jpg?v=636543132137970000",
+     cantidad: 1,
+     },
+ ]
